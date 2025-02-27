47d66d35ef390700dd9a7768dc14d57548777317
Bilel Najjar <bilel.najjar2013@gmail.com>
2025-02-27 16:39:04 +0100

    implement MVVM+ Clean architecture

---
+++ app/src/main/java/com/test/newsappjetpack/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package com.test.newsappjetpack.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RepositoryModule {
+}
---
+++ app/src/main/java/com/test/newsappjetpack/domain/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.test.newsappjetpack.domain.repository
+
+
+interface UserRepository {
+
+
+}
